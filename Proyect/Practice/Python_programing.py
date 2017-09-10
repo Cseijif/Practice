be5983f01a4cd49489c1730c400fd5ac80e61332
Seiji <seijifujii0mail.com>
2017-09-10 17:10:20 -0500

    lel

--- Proyect/Practice/Python_programing.py
+++ Proyect/Practice/Python_programing.py
@@ -2,4 +2,34 @@ Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)]
 Type "copyright", "credits" or "license()" for more information.
 >>> print ("hello world")
 hello world
->>> 
+>>>
+name="Seiji"
+>>> print (name)
+Seiji
+>>> name = 15
+>>> name<-15
+False
+>>> name
+15
+>>> "seijie"
+'seijie'
+>>> Pita="Puto"
+>>> Pita
+'Puto'
+>>> quote="\"Always Remeber , you are unique"
+>>> multiline=''' Just like everyone else'''
+>>> print('i like this',quote+multiline)
+i like this "Always Remeber , you are unique Just like everyone else
+>>> print("%s %s %s" %('i like', quote, multiline))
+i like "Always Remeber , you are unique  Just like everyone else
+>>> print('\n'*5)
+
+
+
+
+
+
+>>> Lista_dejogos=["Mordor","AC","TTW","AOE","C&C"]
+>>> Lista_dejogos[0]
+'Mordor'
+>>>
